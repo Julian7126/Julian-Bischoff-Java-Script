4c26cfde7ea0a10974f7fa5989eeb7a21b8e5cb8
julian7126 <julibischoff@gmail.com>
2022-07-27 15:59:36 -0300

    cuarto_commit

--- codigosimulador.js
+++ codigosimulador.js
@@ -24,14 +24,24 @@ let nuevosUsuarios= 0;
 //         alert("te recomendamos sacar nuestra credencial! tenes el link en nuestra pagina principal");
 
 //     }
+   
+let ingresarNombre=prompt("Nombre y Apellido");
 
-let cualMedico=prompt("Que especialista quiere ver?");
-            if (cualMedico = "cardiologia") {
                 for (let i = 1; i <= 10 ; i++) {
-                    let ingresarNombre = prompt("Ingresar nombre");
+                    let cualMedico=prompt("Que especialista quiere ver?");
+                    if ((cualMedico === "cardiologia") && (cualMedico === "Cardiologia")) {
                     alert(" Turno  N° "+i+" Nombre: "+ingresarNombre+"va a ser asistido por"+cardiologia);
+                    }else if ((cualMedico === "neurologo") && (cualMedico === "Neurologo")){
+                    alert(" Turno  N° "+i+" Nombre: "+ingresarNombre+"va a ser asistido por"+neurologo); 
+                    }else if ((cualMedico === "Dermatologo") && (cualMedico === "Dermatologo")){
+                        alert(" Turno  N° "+i+" Nombre: "+ingresarNombre+"va a ser asistido por"+dermatologo);
+                    }else if ((cualMedico === "traumatologo") && (cualMedico === "Traumatologo")){
+                            alert(" Turno  N° "+i+" Nombre: "+ingresarNombre+"va a ser asistido por"+dermatologo);
+                    }else{
+                        alert("No tenemos esa especialidad en esta CLINICA , Mil disculpas");
+
+                    }                  
                 }
-                
-            }
+            
 
 
