18d64133b6d07a45cc63e00a9a382afdcfd24835
julian7126 <julibischoff@gmail.com>
2022-07-29 11:01:40 -0300

    quinto_commit

--- codigosimulador.js
+++ codigosimulador.js
@@ -1,47 +1,58 @@
-let medicos = cardiologia + neurologo + dermatologo +traumatologo;
-
-
 
 let cardiologia = "mariano bischoff";
 let neurologo = "maricel liendo";
 let dermatologo= "lara bischoff";
 let traumatologo= "julian bischoff";
+let ingresarNombre=prompt("Ingresar Nombre y Apellido");
+let cliente =prompt("Sos cliente?");
 
 
-let turnos= 0;
+function sosCliente(){
+    if ((cliente = "si") || (cliente = "Si")) {
+    
+    alert("GENIAL , cuentas con todos nuestro beneficios! VAS A ESPERAR MENOS TIEMPO PARA TU CONSULTA");
+    
+    }else{
+    
+    alert("te recomendamos sacar nuestra credencial! tenes el link en nuestra pagina principal");
+    
+    }
 
-let nuevosUsuarios= 0;
+}
 
-
-
-
-
-
-// let cliente =prompt("Sos cliente?");
-//     if ((cliente = "si") && (cliente = "Si")) {
-//         alert("GENIAL , cuentas con todos nuestro beneficios! VAS A ESPERAR MENOS TIEMPO PARA TU CONSULTA!");
-//     }else{
-//         alert("te recomendamos sacar nuestra credencial! tenes el link en nuestra pagina principal");
-
-//     }
-   
-let ingresarNombre=prompt("Nombre y Apellido");
-
-                for (let i = 1; i <= 10 ; i++) {
-                    let cualMedico=prompt("Que especialista quiere ver?");
-                    if ((cualMedico === "cardiologia") && (cualMedico === "Cardiologia")) {
-                    alert(" Turno  N° "+i+" Nombre: "+ingresarNombre+"va a ser asistido por"+cardiologia);
-                    }else if ((cualMedico === "neurologo") && (cualMedico === "Neurologo")){
-                    alert(" Turno  N° "+i+" Nombre: "+ingresarNombre+"va a ser asistido por"+neurologo); 
-                    }else if ((cualMedico === "Dermatologo") && (cualMedico === "Dermatologo")){
-                        alert(" Turno  N° "+i+" Nombre: "+ingresarNombre+"va a ser asistido por"+dermatologo);
-                    }else if ((cualMedico === "traumatologo") && (cualMedico === "Traumatologo")){
-                            alert(" Turno  N° "+i+" Nombre: "+ingresarNombre+"va a ser asistido por"+dermatologo);
-                    }else{
-                        alert("No tenemos esa especialidad en esta CLINICA , Mil disculpas");
-
-                    }                  
-                }
-            
+sosCliente()
 
 
+function aplicarDescuentosEnBar(){
+
+let descuentoEnBar= 10%
+
+console.log("Por ser Cliente tienes un "+ "" + descuentoEnBar + " "+ "de DESCUENTO!");
+
+}
+
+function asociados() {
+
+    let cualMedico=prompt("Que especialista quiere ver?");
+
+    for (let i=1; i < 3; i++) {
+
+    if ((cualMedico === "cardiologia") || (cualMedico === "Cardiologia")) {
+        alert(" Turno N° "+i+" Nombre: "+ingresarNombre+"va a ser asistido por"+cardiologia);
+    }
+    else if ((cualMedico === "neurologo") || (cualMedico === "Neurologo")){
+        alert(" Turno N° "+i+" Nombre: "+ingresarNombre+"va a ser asistido por"+neurologo);
+    }
+    else if ((cualMedico === "dermatologo") || (cualMedico === "Dermatologo")){
+        alert(" Turno N° "+i+" Nombre: "+ingresarNombre+"va a ser asistido por"+ dermatologo);
+    }
+    else if ((cualMedico === "traumatologo") || (cualMedico === "Traumatologo")){
+        alert(" Turno N° "+i+" Nombre: "+ingresarNombre+"va a ser asistido por"+dermatologo);
+    }
+    else{
+        alert("No tenemos esa especialidad en esta CLINICA , Mil disculpas");        
+    }
+    }
+}
+
+ asociados()
