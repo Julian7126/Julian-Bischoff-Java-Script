7dc889a5f30c0fcb4cdc43a8e1a81585a5ca53cf
julian7126 <julibischoff@gmail.com>
2022-08-11 11:45:43 -0300

    commit_segunda_rama

--- codigosimulador.js
+++ codigosimulador.js
@@ -1,67 +1,32 @@
-const medicos=[
-    {       
-        nombre:"Mariano Bischoff",
-        Especialidad:"cardiologia",
-        edad:58,
-        Titulo:"Hospital Cardiologico"
-    },
+class Medico{
+    constructor(nombre, especialidad, edad, Titulo) {
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+        this.edad = edad;
+        this.Titulo = Titulo;
+    }
+}
+const medicos = [];//array de medicos
+medicos.push(new Medico("Mariano Bischoff","cardiologia", 58, "Hospital Cardiologico"));
+medicos.push(new Medico("Maricel Liendo","neurologia" , 46, "UNC"));
+medicos.push(new Medico("Lara Bischoff", "dermatologia" , 32, "UNC"));
+medicos.push(new Medico("Julian Bischoff","traumatologo", 38, "Hospital Privado"));
+medicos.push(new Medico("Sol Pucheta","psiquiatria", 35, "UNC"));
+console.table(medicos);
 
 
-    {
-        nombre:"Maricel Liendo",
-        Especialidad:"neurologia",
-        edad:46,
-        Titulo:"UNC"
 
-    },
-
-
-    {
-        nombre:"Lara Bischoff",
-        Especialidad:"nermatologia",
-        edad:31,
-        Titulo:"UNC"
-
-
-    },
-
-    {
-        nombre:"Julian Bischoff",
-        Especialidad:"traumatologo",
-        edad:38,
-        Titulo:"Hospital Privado"
-
-
-    },
-
-];
-
-medicos.push({
-    nombre:"Sol Pucheta",
-    Especialidad:"psiquiatria",
-    edad:34,
-    Titulo:"UNC"
-
-
-});
-
-for(const medico of medicos){
-    console.log("Nombre del Medico:"+"\n"+medico.nombre +"\n"+" Especialidad"+"\n"+ medico.Especialidad);
+for(const med of medicos){
+    console.log("Nombre del Medico:"+"\n"+ medicos.nombre +"\n"+" Especialidad"+"\n"+ medicos.Especialidad);
 
 
 }
 
-
-let cardiologia = "mariano bischoff";
-let neurologo = "maricel liendo";
-let dermatologo= "lara bischoff";
-let traumatologo= "julian bischoff";
 let ingresarNombre=prompt("Ingresar Nombre y Apellido");
-let cliente =prompt("Sos cliente?");
-
+let cliente =prompt("Sos cliente?").toLowerCase();
 
 function sosCliente(){
-    if ((cliente = "si") || (cliente = "Si")) {
+    if (cliente = "si") {
     
     alert("GENIAL , cuentas con todos nuestro beneficios! VAS A ESPERAR MENOS TIEMPO PARA TU CONSULTA");
     
@@ -84,29 +49,21 @@ console.log("Por ser Cliente tienes un "+ "" + descuentoEnBar + " "+ "de DESCUEN
 
 }
 
-function asociados() {
 
-    let cualMedico=prompt("Que especialista quiere ver?");
 
-    for (let i=1; i < 3; i++) {
-
-    if ((cualMedico === "cardiologia") || (cualMedico === "Cardiologia")) {
-        const resultado = medicos.find(medico => medico.Especialidad === cualMedico);
-        alert(" Turno N° "+i+" Nombre: "+ingresarNombre+""+"va a ser asistido por"+""+ resultado);
-    }
-    else if ((cualMedico === "neurologo") || (cualMedico === "Neurologo")){
-        alert(" Turno N° "+i+" Nombre: "+ingresarNombre+""+"va a ser asistido por"+""+medicos.find(medico => medico.Especialidad));
-    }
-    else if ((cualMedico === "dermatologo") || (cualMedico === "Dermatologo")){
-        alert(" Turno N° "+i+" Nombre: "+ingresarNombre+""+"va a ser asistido por"+""+ medicos.find(medico => medico.Especialidad));
-    }
-    else if ((cualMedico === "traumatologo") || (cualMedico === "Traumatologo")){
-        alert(" Turno N° "+i+" Nombre: "+ingresarNombre+""+"va a ser asistido por"+""+medicos.find(medico => medico.Especialidad));
-    }
-    else{
-        alert("No tenemos esa especialidad en esta CLINICA , Mil disculpas");        
-    }
-    }
+function buscarMedico(nombre,especialidad){
+   
+    const encontrado=medicos.find((med)=>med.especialidad === medicos);
 }
 
- asociados()
+
+ for(let i=0;i<2;i++){
+        let especialidad=prompt("Que especialista quieres ver?").toLocaleLowerCase();//especialidad queda en minuscula si o si
+        const medEncontrado = buscarMedico(medicos.especialidad);
+        if(medEncontrado==undefined){
+           alert("No tenemos esa especialidad en esta clinica!")
+        }else{
+            alert(" Turno N° "+i+" Nombre: "+ingresarNombre+""+"va a ser asistido por"+""+ medEncontrado);
+        }
+ }
+
