fccbfdef33b9a0a62e6ae473e3c8906aa6358bc4
julian7126 <julibischoff@gmail.com>
2022-08-31 12:04:55 -0300

    agregando evento

--- codigo.js
+++ codigo.js
@@ -1,11 +1,11 @@
 
 class Medico{
-    constructor(nombre, especialidad, edad, titulo) {
-        this.nombre = nombre;
-        this.especialidad = especialidad;
-        this.edad = edad;
-        this.titulo = titulo;
-    }
+  constructor(nombre, especialidad, edad, titulo) {
+      this.nombre = nombre;
+      this.especialidad = especialidad;
+      this.edad = edad;
+      this.titulo = titulo;
+  }
 }
 const medicos = [];//array de medicos
 medicos.push(new Medico("Mariano Bischoff","cardiologia", 58, "Hospital Cardiologico"));
@@ -17,47 +17,31 @@ console.table(medicos);
 
 const enJSON  = JSON.stringify(medicos);
 
-const DateTime = luxon.DateTime;
-const inicio = DateTime.now();
-console.log(inicio.toString());
-
-// Aplicador de descuentos para el futurp
-
-// function aplicarDescuentosEnBar(){
-
-// let descuentoEnBar= 10%
-
-// alert("Por ser Cliente tienes un "+ "" + descuentoEnBar + ""+ "de DESCUENTO!");
-
-// }
-
-// aplicarDescuentosEnBar();
-
 
 // Funcion para buscar especialista en la class (array)- 
 
 let botonUsuario= document.getElementById("ingresarUsuario");
 ingresarUsuario.onClick=()=>{
-    Swal.fire({
-        title: 'Formulario de Login!',
-        html: `<input type="text" id="login" class="swal2-input" placeholder="Username">
-        <input type="password" id="password" class="swal2-input" placeholder="Password">`,
-        confirmButtonText: 'Ingresar',
-        focusConfirm: false,
-        preConfirm: () => {
-          const login = Swal.getPopup().querySelector('#login').value
-          const password = Swal.getPopup().querySelector('#password').value
-          if (!login || !password) {
-            Swal.showValidationMessage(`Ingresar Usuario y Contrasaseña`)
-          }
-          return { login: login, password: password }
+  Swal.fire({
+      title: 'Formulario de Login!',
+      html: `<input type="text" id="login" class="swal2-input" placeholder="Username">
+      <input type="password" id="password" class="swal2-input" placeholder="Password">`,
+      confirmButtonText: 'Ingresar',
+      focusConfirm: false,
+      preConfirm: () => {
+        const login = Swal.getPopup().querySelector('#login').value
+        const password = Swal.getPopup().querySelector('#password').value
+        if (!login || !password) {
+          Swal.showValidationMessage(`Ingresar Usuario y Contrasaseña`)
         }
-      }).then((result) => {
-        Swal.fire(`
-          Login: ${result.value.login}
-          Password: ${result.value.password}
-        `.trim())
-      });
+        return { login: login, password: password }
+      }
+    }).then((result) => {
+      Swal.fire(`
+        Login: ${result.value.login}
+        Password: ${result.value.password}
+      `.trim())
+    });
 }      
 
 
@@ -67,14 +51,20 @@ ingresarUsuario.onClick=()=>{
 
 
 // ACA EMPIEZA TURNERO
-    let especialidadEncontrada=
+  
 
-function buscarMedico(medicos,especialidad){
-   
-    const encontrado=medicos.find((med)=>med.especialidad == especialidad);
-    return especialidadEncontrada;
+
+function buscarMedico() {
+
+   const encontrado = medicos.filter(med => {
+      if(med.especialidad == 'cardiologia') {
+        console.log('hola')
+      } 
+   })
+
 }
 
+  
 
 
 //  Div inyectado con dom , no funciona
@@ -96,14 +86,14 @@ let tabla=document.createElement("table");
 tabla.className="table table-dark table-striped";
 let tablaBody=document.createElement("tbody");
 for(const meds of medicos){
-    tablaBody.innerHTML+=`
-        <tr>
-            <td>${medicos.nombre}</td>
-            <td>${medicos.especialidad}</td>
-            <td>${medicos.edad}</td>
-            <td>${medicos.titulo}</td>
-        </tr>       
-    `;
+  tablaBody.innerHTML+=`
+      <tr>
+          <td>${medicos.nombre}</td>
+          <td>${medicos.especialidad}</td>
+          <td>${medicos.edad}</td>
+          <td>${medicos.titulo}</td>
+      </tr>       
+  `;
 }
 
 tabla.append(tablaBody);
@@ -120,21 +110,22 @@ articuloTable.append(tabla);
 
 let botonTurnero = document.getElementById("buttonTur");
 buttonTur.onclick=()=>{
-    function asociados (){
-        for(let i=0;i<2;i++){
-               let especialidad=prompt("Que especialista quieres ver?").toLowerCase();
-               const medEncontrado = buscarMedico(especialidadEncontrada);
-               if(medEncontrado==undefined){
-                let contenedor = document.getElementById("TurneroOnline");
-                Swal.fire('No tenemos esa especialidad en este Sanatorio!');
-               }else{
-                Swal.fire(" Turno N° "+i+" Nombre: "+ingresarNombre+""+"va a ser asistido por"+""+ medEncontrado);
-               }
-          }
-       
-       }
-       
-       asociados();      
+  function asociados (){
+      for(let i=0;i<2;i++){
+             let especialidad =prompt("Que especialista quieres ver?").toLowerCase();
+             const medEncontrado = buscarMedico();
+  
+             if(medEncontrado == undefined){
+              // let contenedor = document.getElementById("TurneroOnline");
+              Swal.fire('No tenemos esa especialidad en este Sanatorio!');
+             }else{
+              Swal.fire(" Turno N° "+i+" Nombre: "+""+"va a ser asistido por"+""+ medEncontrado);
+             }
+        }
+     
+     }
+     
+     asociados();      
 }
 
 // boton para preguntar si es cliente
@@ -143,43 +134,43 @@ let botonCliente = document.getElementById("buttonCli");
 
 buttonCli.onClick=()=>{
 
-    function sosCliente() { 
-        let cliente =prompt("Sos cliente?").toLowerCase();
-        
-     if (cliente == "si"){
-                for (let i = 0; i < localStorage.length; i++) 
-                
-                                            {
-                                                let clave = localStorage.key(i);
-                                                console.log("Clave: "+ clave);
-                                                console.log("Valor: "+ localStorage.getItem(clave));
-                                            }
+  function sosCliente() { 
+      let cliente =prompt("Sos cliente?").toLowerCase();
+      
+   if (cliente == "si"){
+              for (let i = 0; i < localStorage.length; i++) 
+              
+                                          {
+                                              let clave = localStorage.key(i);
+                                              console.log("Clave: "+ clave);
+                                              console.log("Valor: "+ localStorage.getItem(clave));
+                                          }
 
-                    let contenedor = document.getElementById("VerificarCliente");
-                    contenedor.innerHTML= "<h2> GENIAL , cuentas con todos nuestro beneficios! VAS A ESPERAR MENOS TIEMPO PARA TU CONSULTA </h2>";
-             }else{
-        Swal.fire({
-            title: '<strong>A sacar nuestra Credit! </strong>',
-            icon: 'info',
-            html:
-              'You can use <b>bold text</b>, ' +
-              '<a href="http://127.0.0.1:5500/index.html">links</a> ',
-            showCloseButton: true,
-            showCancelButton: true,
-            focusConfirm: false,
-            confirmButtonText:
-              '<i class="fa fa-thumbs-up"></i> Great!',
-            confirmButtonAriaLabel: 'Thumbs up, great!',
-            cancelButtonText:
-              '<i class="fa fa-thumbs-down"></i>',
-            cancelButtonAriaLabel: 'Thumbs down'
-             });
-            }
+                  let contenedor = document.getElementById("VerificarCliente");
+                  contenedor.innerHTML= "<h2> GENIAL , cuentas con todos nuestro beneficios! VAS A ESPERAR MENOS TIEMPO PARA TU CONSULTA </h2>";
+           }else{
+      Swal.fire({
+          title: '<strong>A sacar nuestra Credit! </strong>',
+          icon: 'info',
+          html:
+            'You can use <b>bold text</b>, ' +
+            '<a href="http://127.0.0.1:5500/index.html">links</a> ',
+          showCloseButton: true,
+          showCancelButton: true,
+          focusConfirm: false,
+          confirmButtonText:
+            '<i class="fa fa-thumbs-up"></i> Great!',
+          confirmButtonAriaLabel: 'Thumbs up, great!',
+          cancelButtonText:
+            '<i class="fa fa-thumbs-down"></i>',
+          cancelButtonAriaLabel: 'Thumbs down'
+           });
+          }
 
-        }
-        sosCliente();
-    }
-    
+      }
+      sosCliente();
+  }
+  
 
 
 
